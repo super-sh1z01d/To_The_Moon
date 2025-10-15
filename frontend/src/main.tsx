fe218c3fca5e54674e70b8789731fe08a839c6d5
super-sh1z01d <sh1z01d@proton.me>
2025-10-15 11:30:37 +0300

    fix: Simplify routing to use unified BrowserRouter
    - Remove conditional rendering in main.tsx that caused white screen
    - Use single BrowserRouter without basename
    - All routes now properly handled by App.tsx
    - Fixes AuthContext and ModalContext not being available on landing page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,37 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
-import Landing from './pages/Landing'
 import './index.css'
 
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from './lib/queryClient'
-import { LanguageProvider } from './components/layout/LanguageProvider'
-import { ThemeProvider } from './components/layout/ThemeProvider'
-import { Toaster } from './components/ui/sonner'
-
 const rootElement = document.getElementById('root')!
-const path = window.location.pathname
 
-if (path === '/' || path === '') {
-  createRoot(rootElement).render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <LanguageProvider defaultLanguage="en">
-          <ThemeProvider defaultTheme="system">
-            <Landing />
-            <Toaster />
-          </ThemeProvider>
-        </LanguageProvider>
-      </QueryClientProvider>
-    </React.StrictMode>
-  )
-} else {
-  createRoot(rootElement).render(
-    <React.StrictMode>
-      <BrowserRouter basename="/app">
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  )
-}
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
